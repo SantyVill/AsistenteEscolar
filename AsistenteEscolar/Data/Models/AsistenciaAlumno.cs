411e484ff37fea38d100f5a5e16f92fd7c43917f
SantyVillagra <santyvillagra@gmail.com>
2023-06-19 00:54:21 -0300

    Inicio Proyecto Asistente Escolar

---
+++ AsistenteEscolar/Data/Models/AsistenciaAlumno.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsistenteEscolar.Data.Models
+{
+    internal class AsistenciaAlumno
+    {
+    }
+}
---
+++ AsistenteEscolar/Views/InstitucionesViews/InstitucionEdit.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AsistenteEscolar.Views.InstitucionesViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InstitucionEdit : ContentPage
+    {
+        public InstitucionEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
